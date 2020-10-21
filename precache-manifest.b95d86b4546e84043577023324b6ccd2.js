122b5ae91f22033a31b381dec49dae0fc940dcfd
Rick Morrison <morrison723@gmail.com>
2020-10-21 03:56:07 -0600

    Updates

---
+++ precache-manifest.b95d86b4546e84043577023324b6ccd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6d75960841febd81550b1141f34e86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82f1c7fce68f256c6a30",
+    "url": "/static/css/main.5ec25c18.chunk.css"
+  },
+  {
+    "revision": "1631bd85c4ddf4054421",
+    "url": "/static/js/2.577e8c21.chunk.js"
+  },
+  {
+    "revision": "60340bbd69ba9570a0ce069546961ca3",
+    "url": "/static/js/2.577e8c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82f1c7fce68f256c6a30",
+    "url": "/static/js/main.20c6f684.chunk.js"
+  },
+  {
+    "revision": "47f30013799021e6c031",
+    "url": "/static/js/runtime-main.df20a84d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
